10071959444aee0880be14e8f19c2904c05828c0
David BOUCHÉ <david@clicproxy.com>
2017-03-19 21:50:43 +0100

    Ajout du script d'installation des packages Atom.

---
+++ atom-packages-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Minimap
+apm install  minimap minimap-autohide minimap-highlight-selected
+
+# Linter
+apm install linter linter-csslint linter-htmlhint linter-jshint linter-markdownlint linter-php linter-scss-lint linter-twig linter-js-yaml
+
+# Git
+apm install git-plus
+
+# Completion
+apm install autocomplete-modules emmet less-than-slash docblockr
+
+# Esthétique
+apm install file-icons atom-beautify color-picker pigments
