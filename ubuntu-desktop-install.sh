79a1a38d06d6cbd4e104fb709f10064a30849201
David BOUCHÉ <david@clicproxy.com>
2017-03-22 15:34:23 +0100

    Packages renommé, rajout de l'arret à la première erreur, suppression mongodb et installation gnome

--- ubuntu-desktop-install.sh
+++ ubuntu-desktop-install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
+set -e
 ## Installation de mon environnement pour Ubuntu 16.10 Gnome 
 
 # Powerline-shell (a besoin de Python, déjà installé ?)
 sudo git clone https://github.com/powerline/fonts.git /usr/local/share/powerline-fonts
 cd /usr/local/share/powerline-fonts
 sudo ./install.sh
-./install.sh
 
 sudo git clone https://github.com/banga/powerline-shell.git /usr/local/share/powerline-shell
 cd /usr/local/share/powerline-shell
@@ -25,22 +24,13 @@ if [ "xterm-256color" != "linux" ]; then
     PROMPT_COMMAND="_update_ps1; __vte_prompt_command"
 fi
 
-# Alias
-alias l='ls -lh'
-alias ll='ls -lah'
-
 EOT
 
 sudo cat <<EOT >> ~/.bashrc
 
 # Powerline-shell
 function _update_ps1() {
-    PS1="$(powerli
-    # Atom
-    apm install  minimap minimap-autohide minimap-highlight-selected atom-beautify color-picker
-    apm install linter linter-csslint linter-htmlhint linter-jshint linter-markdownlint linter-php linter-scss-lint linter-twig linter-js-yaml file-icons git-plus
-    apm install less-than-slash docblockr pigments
-    apm install autocomplete-modules emmetne-shell.py --colorize-hostname --cwd-max-depth 4  $? 2> /dev/null)"
+    PS1="$(powerline-shell.py --colorize-hostname --cwd-max-depth 4  $? 2> /dev/null)"
 }
 
 if [ "xterm-256color" != "linux" ]; then
@@ -54,9 +44,6 @@ alias ll='ls -lah'
 EOT
 
 sudo apt-get upgrade
-
-# Gnome 
-sudo apt-get install-y gnome-session-flashback
 
 # MC
 sudo apt install -y mc
@@ -77,19 +64,24 @@ sudo systemctl enable mysql
 sudo systemctl restart mysql
 
 # PostgreSQL
-sudo apt install -y php-pgsql postgresql-server  postgresql-contrib
+sudo apt install -y postgresql
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
 sudo postgresql-setup --initdb --unit postgresql
-sudo apt install phpPgAdmin
+sudo apt install phppgadmin
 
 # MongoDB
-sudo apt install -y mongodb-server
-sudo systemctl enable mongod
-sudo systemctl start mongod
+#sudo apt install -y mongodb-server
+#sudo systemctl enable mongod
+#sudo systemctl start mongod
 
 # Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin  --filename=composer
 php -r "unlink('composer-setup.php');"
+
+# Configuration Git
+git config --global user.email "david@clicproxy.com"
+git config --global user.name "David BOUCHÉ"
+
