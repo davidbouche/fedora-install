bb5275d46b7524ef71cfaab8b138f065a58ba801
David BOUCHÉ <david@clicproxy.com>
2017-03-22 16:42:12 +0100

    renomme chromium, supprimer le setup postgresql & change le répertoire courant pour l'installation de composer

--- ubuntu-desktop-install.sh
+++ ubuntu-desktop-install.sh
@@ -44,7 +44,7 @@ sudo apt-get upgrade -y
 sudo apt install -y mc
 
 # Navigateur
-sudo apt install -y chromium
+sudo apt install -y chromium-browser
 
 # Apache PHP
 sudo apt install -y apache2 php php-cli php-xml php-mcrypt php-gd php-pdo php-curl
@@ -62,7 +62,6 @@ sudo systemctl restart mysql
 sudo apt install -y postgresql
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
-sudo postgresql-setup --initdb --unit postgresql
 sudo apt install -y phppgadmin
 
 # MongoDB
@@ -71,6 +70,7 @@ sudo apt install -y phppgadmin
 #sudo systemctl start mongod
 
 # Composer
+cd /tmp
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin  --filename=composer
