0b8d258d9022f79cb09906c19513c424bc7d7d71
David BOUCHÉ <david@clicproxy.com>
2017-03-24 18:09:11 +0100

    Arret sur erreur et suppression des doubles esperluettes.

--- mysql-database-create.sh
+++ mysql-database-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 
 if [ -n "$1" ]; then
     DB="$1"
@@ -13,9 +13,9 @@ PASSWORD=$(php -r "echo substr(str_shuffle(str_repeat('0123456789abcdefghijklmno
 read -s -p "Enter MySQL password: " MYSQLROOTPW
 echo "\n"
 
-mysqladmin -uroot -p$MYSQLROOTPW create "$DB" &&
+mysqladmin -uroot -p$MYSQLROOTPW create "$DB"
 mysql -uroot -p$MYSQLROOTPW -e "GRANT ALL ON $DB.* TO $USER IDENTIFIED BY '$PASSWORD';"
-&& cat << EndOfMessage
+cat << EndOfMessage
 MYSQL_ADDON_DB="$DB"
 MYSQL_ADDON_HOST="localhost"
 MYSQL_ADDON_USER="$USER"
